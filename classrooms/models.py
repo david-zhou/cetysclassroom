d7e31d47dcfefc2e259f3185952ad68e98eeb417
david-zhou <david.zhou.tan@gmail.com>
2014-03-23 19:21:15 -0700

    coordenadas en modelos

--- classrooms/models.py
+++ classrooms/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Salon(models.Model):
 	Name = models.CharField(max_length=30)
 	Image = models.CharField(max_length=10)
+	CoordX = models.IntegerField()
+	CoordY = models.IntegerField()
 	
-# Create your models here.
+
