aeed9d8c4697558e000844cde751709d3eb471af
david-zhou <david.zhou.tan@gmail.com>
2014-03-23 20:19:51 -0700

    base de datos y dos salones

--- classrooms/models.py
+++ classrooms/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
-class Salon(models.Model):
-	Name = models.CharField(max_length=30)
-	Image = models.CharField(max_length=10)
-	CoordX = models.IntegerField()
-	CoordY = models.IntegerField()
-	
+class Classroom(models.Model):
+	Classroom_Name = models.CharField(max_length=30)
+	Classroom_Image = models.CharField(max_length=10)
+	Classroom_CoordX = models.IntegerField()
+	Classroom_CoordY = models.IntegerField()
+
+class Tags(models.Model):
+	Tag_Name = models.CharField(max_length=20)
+
+class Classroom_Tags(models.Model):
+	Tag_ID = models.ForeignKey(Tags)
+	Classroom_ID = models.ForeignKey(Classroom)
 
