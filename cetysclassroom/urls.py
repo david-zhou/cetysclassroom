8e648f3f1c57610dffb94be0eaa7cb4f2d655827
EstebanLFalcon <estebanlf92@gmail.com>
2014-05-05 00:03:23 -0700

    todo bien

--- cetysclassroom/urls.py
+++ cetysclassroom/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'cetysclassroom.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 	url(r'^$','classrooms.views.v_index'),
+	url(r'^(?P<Classroom>\w+)?$','classrooms.views.v_classroom'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- classrooms/views.py
+++ classrooms/views.py
@@ -5,3 +5,6 @@ from django.template import RequestContext
 # Create your views here.
 def v_index(request):
 	return render_to_response("index.html", context_instance = RequestContext(request))
+
+def v_classroom(request,Classroom):
+	return render_to_response("room.html", {"roomNumber":Classroom}, context_instance = RequestContext(request))
